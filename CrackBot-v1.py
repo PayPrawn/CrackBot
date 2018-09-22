84abd653c83ad909570df7a44d9400f191241501
PayPrawn <finlay.scott@rocketmail.com>
2018-09-22 20:25:26 +0100

    made changes

--- CrackBot-v1.py
+++ CrackBot-v1.py
@@ -1,7 +1,7 @@
-import urllib.request
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen('http://www.supremenewyork.com/shop/all')
-page = BeautifulSoup(html.read(), 'html.parser')
-string = str(page)
+import urllib
+url = 'https://www.supremenewyork.com/shop/all'
+page = urllib.urlopen(url) 
+soup = BeautifulSoup(page, 'html.parser')
+string = str(soup)
 print(string)
