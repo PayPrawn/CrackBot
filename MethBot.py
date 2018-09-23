b93f9da7404bce0480094754355489592d62d9a1
PayPrawn <finlay.scott@rocketmail.com>
2018-09-23 09:07:12 +0100

    lolol

--- MethBot.py
+++ MethBot.py
@@ -1,8 +1,16 @@
-from bs4 import BeautifulSoup
-import urllib
-url = 'https://www.supremenewyork.com/shop/all'
-page = urllib.urlopen(url) 
-soup = BeautifulSoup(page, 'html.parser')
-string = str(soup)
-print(string)
-hello
+import requests
+category = 't-shirts'
+#this will be an input
+#keyword = 't-shirts'
+response = (requests.get('https://www.supremenewyork.com/shop/all/t-shirts')).text
+x = response.split('<a class="name-link"')
+x = x[1]
+x = x.split('</a>')
+x = x[0]
+x = x.split('">')
+x = x[1]
+print(x)
+
+
+#data = ((((str(response.json())).split("[{'definition':"))[1]).split("'"))[1]
+#https://www.supremenewyork.com/shop/all/(category)
