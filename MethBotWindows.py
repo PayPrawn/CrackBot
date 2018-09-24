1ee120eab337912da7719171de46e4d340b007e9
Angussss <smith.angus02@gmail.com>
2018-09-24 21:46:01 +0100

    H

--- MethBotWindows.py
+++ MethBotWindows.py
@@ -39,17 +39,44 @@ for i in range(number_of_articles):
         searched_c = (((colour_split[i].split('">'))[1]).split('</a>'))[0]
         if (searched_k == keyword) and (searched_c == colour):
             new_url = 'https://www.supremenewyork.com' +  (((keyword_split[i].split('href="'))[1]).split('">'))[0]
-            
+new_url = 'https://www.facebook.com/?stype=lo&jlou=Afd0abyzVM0zHFrMJ0UVYp0FQSQP3264NyzkeLuqRjw73mPnUc3m92dc_pW-xDHW5Mmj3rRdt1PX_XqcYyU550LaQDSMy1g0wcGnoi2fg7pI0g&smuh=50928&lh=Ac8MKM8BnEO0DHnU'        
 driver.get(new_url)
 time.sleep(5)
-driver.find_elements_by_id("button")
+#driver.find_elements_by_id("button")
+name = driver.find_element_by_id('u_0_q')
+name.send_keys("Testing Name")
 
-
-
-
+# Once on checkout
+'''name = driver.find_element_by_id("order_billing_name")
+name.send_keys("Testing Name")
+name = driver.find_element_by_id("order_email")
+name.send_keys("testing@gmail.com")
+name = driver.find_element_by_id("order_tel")
+name.send_keys("01234 567890")
+name = driver.find_element_by_id("bo")
+name.send_keys("123 Test Street")
+name = driver.find_element_by_id("oba3")
+name.send_keys("Testing")
+name = driver.find_element_by_id("order_billing_address_3")
+name.send_keys("More test")
+name = driver.find_element_by_id("order_billing_city")
+name.send_keys("London")
+name = driver.find_element_by_id("order_billing_zip")
+name.send_keys("TE1 1ST")
+name = driver.find_element_by_id("cnb")
+name.send_keys("1234 1234 1234 1234")
+name = driver.find_element_by_id("vval")
+name.send_keys("123")
+name = driver.find_element_by_id("credit_card_type")
+name.send_keys("Visa")
+name = driver.find_element_by_id("credit_card_month")
+name.send_keys("01")
+name = driver.find_element_by_id("credit_card_year")
+name.send_keys("2020")
 
 
 #finds the number of articles, then goes through that number looking for the 'keyword' data. Then it traces back to the url and takes that. 
 
 #data = ((((str(response.json())).split("[{'definition':"))[1]).split("'"))[1]
 #https://www.supremenewyork.com/shop/all/(category)
+'''
