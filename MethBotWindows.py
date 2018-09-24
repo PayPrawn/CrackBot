49f1167f6a53f44f9c0312768eb1946cba98d4d6
Fin Scott <finlay.scott@rocketmail.com>
2018-09-24 21:43:16 +0100

    Hello

--- MethBotWindows.py
+++ MethBotWindows.py
@@ -3,7 +3,7 @@ import re
 import webbrowser
 import time
 from selenium import webdriver
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
 driver = webdriver.Chrome(executable_path='C:\SeleniumDrivers\chromedriver.exe')
 
 
@@ -39,10 +39,36 @@ for i in range(number_of_articles):
         searched_c = (((colour_split[i].split('">'))[1]).split('</a>'))[0]
         if (searched_k == keyword) and (searched_c == colour):
             new_url = 'https://www.supremenewyork.com' +  (((keyword_split[i].split('href="'))[1]).split('">'))[0]
-            
+
 driver.get(new_url)
-time.sleep(5)
-driver.find_elements_by_id("button")
+driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
+
+name = driver.find_element_by_id("order_billing_name")
+name.send_keys("Testing Name")
+name = driver.find_element_by_id("order_email")
+name.send_keys("testing@gmail.com")
+name = driver.find_element_by_id("order_tel")
+name.send_keys("01234 567890")
+name = driver.find_element_by_id("bo")
+name.send_keys("123 Test Street")
+name = driver.find_element_by_id("oba3")
+name.send_keys("Testing")
+name = driver.find_element_by_id("order_billing_address_3")
+name.send_keys("More test")
+name = driver.find_element_by_id("order_billing_city")
+name.send_keys("London")
+name = driver.find_element_by_id("order_billing_zip")
+name.send_keys("TE1 1ST")
+name = driver.find_element_by_id("cnb")
+name.send_keys("1234 1234 1234 1234")
+name = driver.find_element_by_id("vval")
+name.send_keys("123")
+name = driver.find_element_by_id("credit_card_type")
+name.send_keys("Visa")
+name = driver.find_element_by_id("credit_card_month")
+name.send_keys("01")
+name = driver.find_element_by_id("credit_card_year")
+name.send_keys("2020")
 
 
 
@@ -52,4 +78,3 @@ driver.find_elements_by_id("button")
 #finds the number of articles, then goes through that number looking for the 'keyword' data. Then it traces back to the url and takes that. 
 
 #data = ((((str(response.json())).split("[{'definition':"))[1]).split("'"))[1]
-#https://www.supremenewyork.com/shop/all/(category)
