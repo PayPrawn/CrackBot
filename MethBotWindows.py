0183868ce97887718010a62d8d679ebd56dca29d
Angussss <smith.angus02@gmail.com>
2018-09-27 17:12:24 +0100

    H

--- MethBotWindows.py
+++ MethBotWindows.py
@@ -6,10 +6,7 @@ import os
 from selenium import webdriver
 import threading
 from threading import Thread
-<<<<<<< HEAD
-=======
 import sys
->>>>>>> e680d647048b1d4538a9a087adb7777e398e5b10
 
 driver = webdriver.Chrome(executable_path='C:\SeleniumDrivers\chromedriver.exe')
 starttime = time.time()
@@ -94,11 +91,6 @@ def realtest():
     cardmonth = input('expiry month: \n')
     cardyear = input('expiry year: \n')
 
-<<<<<<< HEAD
-def autofill():
-    (driver.find_element_by_xpath('//*[@id="order_billing_name"]')).send_keys(name) 
-    (driver.find_element_by_xpath('//*[@id="order_email"]')).send_keys(email)
-=======
 def fillname():
     (driver.find_element_by_xpath('//*[@id="order_billing_name"]')).send_keys(name)
 
--- UI1.cxx
+++ UI1.cxx
@@ -1,5 +1,4 @@
 if (GetKeyState(VK_LBUTTON) < 0) {
-
 {
 POINT p;
 if (GetCursorPos(&p))
@@ -8,13 +7,10 @@ cout<<"\nSCREEN\nx coord->";
 cout<<p.x;
 cout<<"\ny coord->";
 cout<<p.y;
-
 }
 SetConsoleTitle("paint");
 HWND hWnd;
 hWnd = FindWindow(NULL, "paint");
-
-
 if (ScreenToClient(hWnd, &p));
 {
 cout<<"\n\nWINDOW\nx coord->";
@@ -24,6 +20,5 @@ cout<<p.y;
 }
 int pwx;
 int pwy;
-
 pwx=p.x;
 pwy=p.y;
